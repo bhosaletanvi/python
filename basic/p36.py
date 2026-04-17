fc6a2f4a3c2a953352a1fade8bd89053a6c97fdf
Tanvi <bhosaletanvi59@gmail.com>
2026-04-17 11:50:43 +0530

    file added

---
+++ basic/p36.py
@@ -0,0 +1,11 @@
+person={
+	"name" : "Tanvi",
+	"matches" : 29,
+	"runs" : 400
+	}
+print(person)
+print(id(person))
+
+person["name "] ="Tanvi "
+print(person)
+print(id(person))
---
+++ basic/p37.py
@@ -0,0 +1,7 @@
+set={10,20,30}
+print(set)
+print(id(set))
+
+s.add(60)
+print(set)
+print(id(set))
