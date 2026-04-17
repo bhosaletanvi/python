984008b44f2ff10855decebef7fa7dd45fa882c7
Tanvi <bhosaletanvi59@gmail.com>
2026-04-17 11:46:25 +0530

    file added

---
+++ basic/p34.py
@@ -0,0 +1,10 @@
+ls=[10,20,30,40]
+print("before Change ")
+print(ls)
+print(id(ls))
+
+ls[2]=90
+print("After Change ")
+print(ls)
+print(id(ls))
+
---
+++ basic/p35.py
@@ -0,0 +1,7 @@
+tp=(10,20,30,40)
+print(tp)
+print(id(tp))
+
+tp[1]=60
+print(tp)
+print(id(tp))
