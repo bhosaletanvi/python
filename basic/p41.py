5b3419eebcccffb9a15e3dbc44823b007c0d23fd
Tanvi <bhosaletanvi59@gmail.com>
2026-04-17 11:59:45 +0530

    file added

---
+++ basic/p41.py
@@ -0,0 +1,11 @@
+a=5
+b=2
+c=True or False
+d= True or True
+e= False or False
+f= False or True
+
+print(c)
+print(d)
+print(e)
+print(f)
---
+++ basic/p42.py
@@ -0,0 +1,7 @@
+a=5
+b=2
+c=not(True)
+print(c)
+
+d=not(False)
+print(d)
