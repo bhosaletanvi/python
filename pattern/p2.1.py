43dd5f3cec1bf96b2ed5794076363e310d9cf0c3
Tanvi <bhosaletanvi59@gmail.com>
2026-05-07 23:03:17 +0530

    file added

---
+++ pattern/p2.1.py
@@ -0,0 +1,9 @@
+num=1
+for i in range (0,4) :
+	for j in range(1,5) :
+		print(num,end=" ")
+	if(num==1) :
+		num=0
+	else :
+		num=1
+	print()
---
+++ pattern/p2.py
@@ -0,0 +1,7 @@
+for i in range (0,4) :
+	for j in range(1,5) :
+		if(i%2==1):
+			print("0",end=" ")
+		else:
+			print("1",end=" ")
+	print()
